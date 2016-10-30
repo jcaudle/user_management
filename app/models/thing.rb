ec2b40ab9cf0ea92d072c2bf75a5cf3fb63f47ef
Joseph Caudle <joseph@josephcaudle.com>
2016-10-29 22:15:30 -0400

    Allow users to do things with things

---
+++ app/models/thing.rb
@@ -0,0 +1,3 @@
+class Thing < ApplicationRecord
+  validates_presence_of :name
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029170904) do
+ActiveRecord::Schema.define(version: 20161030004932) do
+
+  create_table "things", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
