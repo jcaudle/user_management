c445f8f52617bf3d9d253b8d2e0fab016db6eb28
Joseph Caudle <joseph@josephcaudle.com>
2016-11-03 23:36:36 -0400

    Make this more comfortable for someone new

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-# README
+# User Management
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This is just a simple Rails app. You should be able to get it configured like any other. To get started, install the bundle and setup the database with rake:
 
-Things you may want to cover:
+```
+$ bundle install && rake db:setup
+```
 
-* Ruby version
+(No further database configuration should be necessary)
 
-* System dependencies
+At that point, you should be able to run the server:
 
-* Configuration
+```
+$ rails server
+```
 
-* Database creation
+You should be able to log in as any of the users created in the seed step with their email address and using the password `password`. For good measure, the following users are created and configured by default:
 
-* Database initialization
+```
+someone@example.com
+reader@example.com
+creator@example.com
+editor@example.com
+destroyer@example.com
+admin@example.com
+```
 
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+There is also a sample Thing created which should be viewable in the index even if not logged in. Each user has different capabilities and the abilities can be modified by the admin user.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if Thing.count == 0
+  Thing.create(name: "Something")
+  puts "Created Something!"
+end
+
+User.create(email: 'someone@example.com', password: 'password', password_confirmation: 'password') unless User.find_by email: 'someone@example.com'
+puts "someone@example.com is configured."
+
+reader = User.find_by(email: 'reader@example.com') || User.create(email: 'reader@example.com', password: 'password', password_confirmation: 'password')
+reader.abilities = :read_things
+reader.save
+puts "reader@example.com is configured."
+
+creator = User.find_by(email: 'creator@example.com') || User.create(email: 'creator@example.com', password: 'password', password_confirmation: 'password')
+creator.abilities = :create_things
+creator.save
+puts "creator@example.com is configured."
+
+editor = User.find_by(email: 'editor@example.com') || User.create(email: 'editor@example.com', password: 'password', password_confirmation: 'password')
+editor.abilities = :modify_things
+editor.save
+puts "editor@example.com is configured."
+
+destroyer = User.find_by(email: 'destroyer@example.com') || User.create(email: 'destroyer@example.com', password: 'password', password_confirmation: 'password')
+destroyer.abilities = :destroy_things
+destroyer.save
+puts "destroyer@example.com is configured."
+
+admin = User.find_by(email: 'admin@example.com') || User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+admin.abilities = :modify_users
+admin.save
+puts "admin@example.com is configured."
